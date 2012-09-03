3f14f837128e3c50428c4aaca088830659a8e241
Nikita Zubkov <zubchick@yandex-team.ru>
2012-09-03 08:59:53 +0400

    fix PATHs

--- zubchick.el
+++ zubchick.el
@@ -89,12 +89,14 @@ to the previously saved position"
 ;; Git
 (autoload 'magit-status "magit" nil t)
 
+
 ;;; PATH
+(setq *PATH* (shell-command-to-string "source $HOME/.zshrc && printf $PATH"))
 (if (not (getenv "TERM_PROGRAM"))
-       (setenv "PATH"
-               (shell-command-to-string "source $HOME/.zshrc && printf $PATH")))
+       (setenv "PATH" *PATH*))
+(setq exec-path (append exec-path (split-string *PATH* ":")))
 
-
+;; slime
 (defun slime-java-describe (symbol-name)
   "Get details on Java class/instance at point."
   (interactive (list (slime-read-symbol-name "Java Class/instance: ")))
