5a63a623cb8d921f44585eb1ae7473d7d188e419
Nikita Zubkov <zubchick@yandex-team.ru>
2012-10-20 20:34:14 +0400

    fix my package list

--- init.el
+++ init.el
@@ -1,9 +1,7 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq 'package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                          ("marmalade" . "http://marmalade-repo.org/packages/")
+                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
 
