e896a67094bfbeb47534ff453128ced504677821
Nikita Zubkov <zubchick@yandex-team.ru>
2012-11-02 14:58:14 +0400

    new command for regenerate TAGS

--- zubchick/projectile.el
+++ zubchick/projectile.el
@@ -1,4 +1,6 @@
 (require 'projectile)
+(setq projectile-tags-command
+      "find %s -type f -print | egrep -v \"/[.][a-zA-Z]\" | etags -")
 (define-key projectile-mode-map (kbd "M-p") 'projectile-find-file)
 (define-key projectile-mode-map (kbd "M-F") 'projectile-grep)
 (projectile-global-mode)
