24bd89106636611f0b18e0823e7067062ff39e24
Nikita Zubkov <zubchick@yandex-team.ru>
2013-03-26 13:29:12 +0400

    projectile

--- zubchick/projectile.el
+++ zubchick/projectile.el
@@ -1,6 +1,11 @@
+;;; for proper TAGS install ctags from brew
 (require 'projectile)
-(setq projectile-tags-command
-      "find %s -type f -print | egrep -v \"/[.][a-zA-Z]\" | etags -")
+
 (define-key projectile-mode-map (kbd "M-p") 'projectile-find-file)
-(define-key projectile-mode-map (kbd "M-F") 'projectile-grep)
+(define-key projectile-mode-map (kbd "M-F") 'projectile-ack)
+(add-hook 'prog-mode-hook
+          (lambda () (require 'popwin)
+            (push '("*Ack-and-a-half*" :height 25)
+                  popwin:special-display-config)))
+
 (projectile-global-mode)
