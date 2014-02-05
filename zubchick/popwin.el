99b16dd501c2afa0a069d78bb03073683b4d1265
Nikita Zubkov <zubchick@yandex-team.ru>
2014-02-05 17:08:31 +0400

    fix magit popwin

--- zubchick/popwin.el
+++ zubchick/popwin.el
@@ -4,7 +4,7 @@
 (setq display-buffer-function 'popwin:display-buffer)
 
 (push '("*Ack-and-a-half*" :height 25) popwin:special-display-config)
-(push '("*magit-process*" :tail t :noselect t)
+(push '("*magit-process*" :tail t)
       popwin:special-display-config)
 (push '("*Shell Command Output*" :tail t :noselect t)
       popwin:special-display-config)
