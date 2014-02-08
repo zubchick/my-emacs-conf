8741fd4b013a3b072a63314bbf0bd809c7d4011a
Nikita Zubkov <zubchick@yandex-team.ru>
2014-02-08 17:50:15 +0400

    delete unused code in zubchick/python.el

--- zubchick/python.el
+++ zubchick/python.el
@@ -7,17 +7,11 @@
   (idle-highlight-mode t)
   (flycheck-mode t)
 
-  ;; (flymake-python-pyflakes-load)
-
   )
 
-;; (autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-hook 'python-mode-hook 'my-python-hook)
 
 ;;; jedi
 (setq jedi:tooltip-method '(pos-tip))
 (add-hook 'python-mode-hook 'auto-complete-mode)
 (add-hook 'python-mode-hook 'jedi:ac-setup)
-
-;; (setq flymake-python-pyflakes-executable "flake8")
-;; (setq flymake-python-pyflakes-extra-arguments '("--ignore=E501,E124,E261"))
