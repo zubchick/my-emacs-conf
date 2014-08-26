bfbb21dff3e3f032a3e47ea783f43e8408984426
Nikita Zubkov <zubchick@yandex-team.ru>
2014-08-26 12:27:39 +0400

    ag in popwin

--- zubchick/popwin.el
+++ zubchick/popwin.el
@@ -4,6 +4,8 @@
 (setq display-buffer-function 'popwin:display-buffer)
 
 (push '("*Ack-and-a-half*" :height 25) popwin:special-display-config)
+(push '("\\*ag search.*\\*" :height 25 :regexp t) popwin:special-display-config)
+
 (push '("*magit-process*" :tail t)
       popwin:special-display-config)
 (push '("*Shell Command Output*" :tail t :noselect t)
