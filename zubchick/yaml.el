cb41722b270241df7116648d43fd79ffdda8d551
Nikita Zubkov <zubchick@yandex-team.ru>
2014-08-26 13:31:04 +0400

    yaml indent 4

--- zubchick/yaml.el
+++ zubchick/yaml.el
@@ -1,6 +1,7 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(setq yaml-indent-offset 4)
 (add-hook 'yaml-mode-hook
           '(lambda ()
              (define-key yaml-mode-map "<enter>" 'newline-and-indent)))
