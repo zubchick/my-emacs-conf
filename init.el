e1736ea444e36f1d08a6310dac68e151b64481e2
Nikita Zubkov <zubchick@yandex-team.ru>
2015-08-13 15:14:44 +0300

    add some repos

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("elpy" . "http://jorgenschaefer.github.io/packages/")
+                         ))
+
+(setq package-check-signature nil)
 
 (package-initialize)
 
