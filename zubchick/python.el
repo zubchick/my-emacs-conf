f49204b23943efaa3161c505a3348a86864c5047
Nikita Zubkov <zubchick@yandex-team.ru>
2015-08-13 15:17:28 +0300

    migration to elpy

--- zubchick/python.el
+++ zubchick/python.el
@@ -5,10 +5,22 @@
         font-lock-maximum-decoration t)
 
   (idle-highlight-mode t)
+  (flymake-mode 0)
   (flycheck-mode t)
-
   (electric-indent-mode 0)
 
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
   )
 
-(add-hook 'python-mode-hook 'my-python-hook)
+(elpy-enable)
+(elpy-use-ipython)
+(add-hook 'elpy-mode-hook 'my-python-hook)
+
+
+(when (require 'flycheck nil t)
+  (setq elpy-default-minor-modes (delete 'flymake-mode elpy-modules)
+        elpy-default-minor-modes (delete 'elpy-module-flymake elpy-modules)
+        elpy-default-minor-modes (delete 'elpy-module-highlight-indentation elpy-modules)
+        )
+
+  (add-to-list 'elpy-modules 'flycheck-mode))
