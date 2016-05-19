2d625fde74e74d3fc5f29bef5a9d17ece2dff1b3
Nikita Zubkov <nzubkov@mirantis.com>
2016-05-19 18:23:49 +0300

    fix ido-mode settings

--- zubchick/ido.el
+++ zubchick/ido.el
@@ -1,5 +1,8 @@
+(require 'ido-ubiquitous)
+
 (setq ido-enable-flex-matching t
-      ido-use-filename-at-point 'guess)
+      ido-create-new-buffer 'always)
 (ido-mode 1)
 (ido-everywhere 1)
+(ido-ubiquitous-mode 1)
 (flx-ido-mode 1)
