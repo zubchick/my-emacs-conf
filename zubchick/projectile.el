4800a3a61b2fcd6a33de8e6deaed7324ea36598f
Nikita Zubkov <nzubkov@mirantis.com>
2016-05-19 18:25:21 +0300

    fix projectile settings

--- zubchick/projectile.el
+++ zubchick/projectile.el
@@ -4,13 +4,11 @@
 (define-key projectile-mode-map (kbd "M-p") 'projectile-find-file)
 (define-key projectile-mode-map (kbd "M-F") 'projectile-ag)
 
-(setq projectile-enable-caching t)
-(setq projectile-tags-command
-      (concat projectile-tags-command
-              " --exclude=\"*.min.*\" --python-kinds=-i"))
+(setq projectile-enable-caching t
+      projectile-tags-command (concat projectile-tags-command
+                                      " --exclude=\"*.min.*\" --python-kinds=-i")
 
-(setq projectile-mode-line-lighter "Project")
+      projectile-mode-line-lighter "Project"
+      projectile-completion-system 'ido)
 
 (projectile-global-mode)
-
-(setq ack-and-a-half-arguments '())     ; хак который нейтрализует баг -_-"
