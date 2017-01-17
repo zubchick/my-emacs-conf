169504e86879cd1c286ca84f4afe60352b17eb34
Nikita Zubkov <zubchick@yandex-team.ru>
2017-01-18 01:07:04 +0300

    add reverse rsync deploy.sh

--- zubchick/deploy.el
+++ zubchick/deploy.el
@@ -3,12 +3,18 @@
 (defun deploy-message (process event)
   (message "Deploing project... Done."))
 
-(defun my-deploy ()
+(defun my-deploy (&rest args)
   (interactive)
   (message "Deploing project... ")
   (save-buffer (current-buffer))
-  (start-process "deploy-process" "*Deploy*" "deploy.sh")
+  (apply 'start-process
+         (append ["deploy-process" "*Deploy*" "deploy.sh"] args))
   (set-process-sentinel (get-process "deploy-process") 'deploy-message))
 
+(defun my-reverse-deploy ()
+  (interactive)
+  (my-deploy "--reverse"))
+
+(global-set-key (kbd "C-c M-r") 'my-reverse-deploy)
 (global-set-key (kbd "C-c M-b") 'my-deploy)
 (add-hook 'after-save-hook 'my-deploy)
