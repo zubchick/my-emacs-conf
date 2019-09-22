40b567bafc3c247e958e47f94baae88c02e99b9e
Nikita Zubkov <zubchick@yandex-team.ru>
2019-09-22 16:31:49 +0300

    Update treemacs config

--- zubchick/treemacs.el
+++ zubchick/treemacs.el
@@ -1,4 +1,21 @@
+(defun my-treemacs-back-and-forth ()
+  (interactive)
+  (if (treemacs-is-treemacs-window-selected?)
+      (aw-flip-window)
+    (treemacs-select-window)))
+
 (use-package treemacs
   :ensure t
+  :defer t
   :init
-  (global-set-key (kbd "M-[") 'treemacs))
+  (treemacs-tag-follow-mode)
+  (global-set-key (kbd "M-[") 'treemacs)
+  (global-set-key (kbd "M-]") 'my-treemacs-back-and-forth))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
